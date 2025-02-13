932bc467bd1ccf21ec49622ab92617fc367551a2
jkmende05 <kohta20@gmail.com>
2025-02-13 11:45:09 -0500

    Updated linear_regression.R

--- obesity_linear_regression/linear_regression.R
+++ obesity_linear_regression/linear_regression.R
@@ -17,16 +17,19 @@ str(obesity_data)
 
 colSums(is.na(obesity_data))
 
+# Filter data to remove rows with NA values in Data Value, Education, or Income
 complete_data <- obesity_data %>%
   filter(!is.na(Data_Value), !is.na(Education), !is.na(Income))
 head(complete_data)
 
+# Get unique education and income values
 unique_educ_values <- unique(complete_data$Education)
 unique_educ_values
 
 unique_income_values <- unique(complete_data$Income)
 unique_income_values
 
+# Get count of number of non-null and unique values in columns
 non_null_counts <- colSums(!is.na(obesity_data))
 unique_counts <- sapply(obesity_data,
                         function(col) length(unique(na.omit(col))))
@@ -44,6 +47,7 @@ pot_predictors <- var_counts %>%
   arrange(desc(non_null_counts))
 pot_predictors
 
+# Scatter plot for sample size and data value
 ggplot(obesity_data, aes(x = Sample_Size, y = Data_Value)) +
   geom_point() +
   labs(
@@ -57,6 +61,7 @@ ggplot(obesity_data, aes(x = Sample_Size, y = Data_Value)) +
   ) +
   scale_x_continuous(labels = scales::comma)
 
+# Scatter plot for correlation between high confidence limit and obesity index
 ggplot(obesity_data, aes(x = High_Confidence_Limit, y = Data_Value)) +
   geom_point() +
   labs(
@@ -71,13 +76,16 @@ ggplot(obesity_data, aes(x = High_Confidence_Limit, y = Data_Value)) +
   ) +
   scale_x_continuous(labels = scales::comma)
 
+# Split, train and test model
 split <- sample.split(complete_data$Data_Value, SplitRatio = 0.7)
 training_set <- subset(complete_data, split == TRUE)
 testset <- subset(complete_data, split == FALSE)
 
+# Get linear model
 lm_model <- lm(Data_Value ~ High_Confidence_Limit, data = training_set)
 summary(lm_model)
 
+# Create predictions and compare with actual results
 predictions <- predict(lm_model, newdata = complete_data)
 results <- data.frame(
   Actual = complete_data$Data_Value,
@@ -85,9 +93,11 @@ results <- data.frame(
 )
 head(results)
 
+# Get calculated r2 value
 r2 <- summary(lm_model)$r.squared
 r2
 
+# Plot data and linear model on ggplot2 scatter plot
 ggplot(complete_data, aes(x = High_Confidence_Limit, y = Data_Value)) +
   geom_point() +
   geom_smooth(method = "lm", col = "blue") +
